1e166acaf8fa09552ac99eb8adff1e53c947e82f
Jonathan <psisquared2@users.noreply.github.com>
2022-03-09 11:03:01 +0100

    improvements to integration test

--- integration_test_setup/pre-run-cleanup.py
+++ integration_test_setup/pre-run-cleanup.py
@@ -23,25 +23,38 @@ def regenerate_test_structure():
     files_per_level = 2
     def generate(dir: Path, level: int):
         for j in range(dirs_per_level):
-            newdir = dir.joinpath(f"S{s}L{level}D{j+1}")
+            newdir = dir.joinpath(f"S{s+1}L{level}D{j+1}")
             os.makedirs(newdir)
             if level < levels:
                 generate(newdir, level+1)
         for j in range(files_per_level):
-            newfile = dir.joinpath(f"S{s}L{level}F{j+1}.txt")
+            newfile = dir.joinpath(f"S{s+1}L{level}F{j+1}.txt")
             with open(newfile, 'w') as file:
                 content = ''.join(random.choice(string.ascii_lowercase) for i in range(100))
                 newfile.write_text(content)
 
+    # generate the two source trees
     currentPath = Path(__file__).parent.resolve()
-    for s in range(1, sources + 1):
-        top = currentPath.joinpath(f"source-{s}")
+    for s in range(sources):
+        top = currentPath.joinpath(f"source-{s+1}")
         if not top.exists():
             generate(top, 1)
-    targetPath = currentPath.joinpath("target")
-    os.makedirs(targetPath, exist_ok=True)
+            
+    # generate the target tree
+    backupPath = currentPath.joinpath("./target")
+    targetPath = backupPath.joinpath("./2022_03_07")
+    if not targetPath.exists():
+        os.makedirs(targetPath)
+        for s in range(sources):
+            source = currentPath.joinpath(f"./source-{s+1}")
+            target = targetPath.joinpath(f"./test-source-{s+1}")
+            shutil.copytree(source, target)
+        shutil.copy2(currentPath.joinpath("metadata-integration-test.json"), targetPath.joinpath("metadata.json"))
+        
+    # make modifications (see TODO above)
     
-    delete_all_but_latest_backup(targetPath)
+    
+    delete_all_but_latest_backup(backupPath)
 
 if __name__ == '__main__':
     regenerate_test_structure()
